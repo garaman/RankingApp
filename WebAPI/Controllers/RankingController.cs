513c8f6d736d6d9a2fb70e8a5ded2e51e12d0519
HyunSoo <ahs20103@gmail.com>
2024-07-01 17:55:55 +0900

    20240701_WebAPI

---
+++ WebAPI/Controllers/RankingController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedData.Models;
+using WebAPI.Data;
+
+namespace WebAPI.Controllers
+{
+    // REST
+    // 공식 표준 스펙은 아님
+    // 원래 있던 HTTP 통신에서 기능을 '재사용'해서데이터 송수신 규칙을 만든것.
+
+
+    // CRLD
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankingController : ControllerBase
+    {
+        ApplicationDbContext _context;
+
+        public RankingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Create
+
+        // Read
+        [HttpGet]
+        public List<GameResult> GetGameResults()
+        {
+            List<GameResult> results = _context.GameResults
+                .OrderByDescending(item => item.Score)
+                .ToList();
+
+            return results;
+        }
+
+        [HttpGet("{id}")]
+        public GameResult GetGameResults(int id)
+        {
+            GameResult? result = _context.GameResults
+                .Where(item => item.Id == id)
+                .FirstOrDefault();
+
+            return result;
+        }
+
+        // Update
+
+        // Delete
+    }
+}
