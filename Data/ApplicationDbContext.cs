9e2890f3f3dc9694ef7d8bcef2e288f3a3e0c80f
HyunSoo <ahs20103@gmail.com>
2024-07-01 12:45:06 +0900

    start

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using RankingApp.Data.Models;
+
+namespace RankingApp.Data
+{    
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        public DbSet<GameResult> GameResults { get; set; }
+    }
+}
---
+++ Data/Service/RankingService.cs
@@ -0,0 +1,69 @@
+﻿using RankingApp.Data.Models;
+
+namespace RankingApp.Data.Service
+{
+    public class RankingService
+    {
+        ApplicationDbContext _context;
+
+        public RankingService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Create
+        public Task<GameResult> AddGameResult(GameResult gameResult)
+        {
+            _context.GameResults.Add(gameResult);
+            _context.SaveChanges();
+
+            return Task.FromResult(gameResult);
+        }
+
+        // Read
+        public Task<List<GameResult>> GetGameResultsAsync()
+        {
+            List<GameResult> results = _context.GameResults
+                .OrderByDescending(item => item.Score)
+                .ToList();
+
+            return Task.FromResult(results);
+        }
+
+        // Update
+        public Task<bool> UpdateGameResult(GameResult gameResult)
+        {
+            var findResult = _context.GameResults
+                .Where(x=>x.Id == gameResult.Id)
+                .FirstOrDefault();
+
+            if (findResult == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            findResult.USerName = gameResult.USerName;
+            findResult.Score = gameResult.Score;
+            _context.SaveChanges();
+
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> DeleteGameResult(GameResult gameResult)
+        {
+            var findResult = _context.GameResults
+                .Where(x => x.Id == gameResult.Id)
+                .FirstOrDefault();
+
+            if (findResult == null)
+            {
+                return Task.FromResult(false);
+            }
+
+            _context.GameResults.Remove(findResult);
+            _context.SaveChanges();
+
+            return Task.FromResult(true);
+        }
+    }
+}
